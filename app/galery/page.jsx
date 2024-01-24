30cc91970016ddf7e03eb7d51cfb83884797632c
irfan-fauzi <irfanwintech@yahoo.com>
2024-01-24 12:54:38 +0700

    init

--- app/galery/page.jsx
+++ app/galery/page.jsx
@@ -1,21 +1,30 @@
-import { getDataGalery } from "@/utils/getDataApi"
-import CardGalery from "../components/CardGalery"
-import Image from "next/image"
+import { getDataGalery } from "@/utils/getDataApi";
+
+import Image from "next/image";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Page = async () => {
-  const galery = await getDataGalery()
+  const galery = await getDataGalery();
   return (
-    <div>
-      {
-       galery.map(item => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <Image src={item.url} width={200} height={200} alt="image"/>
+    <>
+      <Header />
+      <div className="container mx-auto">
+        <div className="grid-card gap-5">
+          {galery.map((item) => (
+            <div
+              key={item.id}
+              className="bg-orange-300 flex items-center justify-center flex-col rounded-xl py-10"
+            >
+              <Image src={item.url} width={200} height={200} alt="image" />
+              <p className="text-center mt-5">{item.title}</p>
+            </div>
+          ))}
         </div>
-       ))
-      }
-    </div>
-  )
-}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Page
+export default Page;
